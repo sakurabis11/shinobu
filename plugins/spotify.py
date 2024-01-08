8774ee9fd92d1868292085a4936a96d0f0488d4e
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-08 22:37:03 +0530

    Update spotify.py

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -9,7 +9,8 @@ from info import LOG_CHANNEL, LOG_CHANNEL as DUMP_GROUP
 # Spotify API credentials (replace with your own)
 client_id = 'd3a0f15a75014999945b5628dca40d0a'
 client_secret = 'e39d1705e35c47e6a0baf50ff3bb587f'
-redirect_uri = 'http://localhost:8080'  # Adjust as needed
+redirect_uri = 'http://localhost:8080' 
+username = '315jpvz6ki4e734f7r6uwvue4ina'
 scope = 'user-library-read'
 
 # Authenticate with Spotify API
