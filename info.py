ebe31d5c4326ef620887636e454d62b799377937
obanaichu <153916736+obanaichu@users.noreply.github.com>
2024-01-09 21:02:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -64,10 +64,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
-auth_channel_2 = environ.get('AUTH_CHANNEL_2')
 auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-AUTH_CHANNEL_2 = int(auth_channel) if auth_channel_2 and id_pattern.search(auth_channel_2) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # This is required for the plugins involving the file system.
