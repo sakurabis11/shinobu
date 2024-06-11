38713e0dbad584fd0f7221500291eef979922cfe
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-06-11 08:29:38 +0530

    Update f_sub.py

--- plugins/f_sub.py
+++ plugins/f_sub.py
@@ -6,11 +6,11 @@ import re
 @Client.on_message(filters.create(not_subscriber))
 async def is_not_subscribed(client, message):
     if message.text.startswith('/'): 
-        buttons = [[InlineKeyboardButton(text="ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ", url=client.invitelink)]]
+        buttons = [[InlineKeyboardButton(text="ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ", url="https://t.me/sd_bots")]]
         text = f"sᴏʀʀʏ {message.from_user.mention}\nʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍʏ ʙᴏᴛ."
         await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
     elif message.text: 
-        buttons = [[InlineKeyboardButton(text="ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ", url=client.invitelink)]]
+        buttons = [[InlineKeyboardButton(text="ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ", url="https://t.me/sd_bots")]]
         text = f"sᴏʀʀʏ {message.from_user.mention}\nʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ᴊᴏɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍʏ ʙᴏᴛ."
         await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
     else:
