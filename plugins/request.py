cf0e95f7c0726b0f1bbb5ddb240ac18003bbfb55
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-06-22 11:05:25 +0530

    Update and rename request.txt to request.py

--- plugins/request.py
+++ plugins/request.py
@@ -7,7 +7,7 @@ from pyromod.exceptions import ListenerTimeout
 import random
 import string
 
-user_status = {}
+
 
 @Client.on_chat_join_request()
 async def auto_request(client: Client, message: ChatMemberUpdated):
@@ -25,19 +25,16 @@ async def auto_request(client: Client, message: ChatMemberUpdated):
 
     password = ''.join(random.choice(all_chars) for _ in range(desired_length))
 
-    user_status.update(user_id=user_id , chat_id=chat_id , title=title , captcha=password)
-    print(user_status)
-    passw = user_status.get('captcha')
-    print(passw)
+    
     chat_type=message.chat.type
     if enums.ChatType.PRIVATE:
      try:
-       c = await client.ask(user_id , f"Hello {message.from_user.mention}\n\n📃 Enter the below 6-digit captcha to join in {title}\n\n📝 Captcha: {str(passw)}\n\n⏳ Time Out: 2 Min (120 Sec)" ,
+       c = await client.ask(user_id , f"Hello {message.from_user.mention}\n\n📃 Enter the below 6-digit captcha to join in {title}\n\n📝 Captcha: {str(password)}\n\n⏳ Time Out: 2 Min (120 Sec)" ,
                          filters=filters.text, timeout=120)
-       print(f"captcha is {c.text}")
-       print(c)
+       
+       
        if user_id == message.from_user.id:
-           if c.text == passw:
+           if c.text == password:
                await client.approve_chat_join_request(chat_id , user_id)
                await client.send_photo(chat_id=user_id , photo="https://telegra.ph/file/abce311b41052c52bc8ec.jpg" ,
                                        caption=f"Hello {message.from_user.mention}\n\nYou're joined in {title} Successfully")
