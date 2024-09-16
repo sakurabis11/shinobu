a3ebd983f09c2b9e2e3128a10a0317de66db4cf5
sakurabis11 <155870850+sakurabis11@users.noreply.github.com>
2024-09-16 23:47:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -602,7 +602,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.photo("https://envs.sh/wNq.jpg")
         await query.message.edit_text(
             text=script.POSTSENDER_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
